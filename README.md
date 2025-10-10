95b11be6948d03af85d3a02761089cd1a37e77b1
Dave Seidman <daveseidman@Daves-MacBook-Pro.local>
2025-10-10 17:06:26 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# 📸 War is Peace - Phootbooth
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, useRef } from 'react';
+import './index.scss';
+
+const App = () => {
+
+  return (
+    <div className="app">
+
+    </div>
+  );
+};
+
+export default App;
